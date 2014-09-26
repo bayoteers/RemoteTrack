8b4346c1d5fadfd2a70162144a4bb50ceea36e30
Pami Ketolainen <pami.ketolainen@jolla.com>
2014-09-26 09:32:07 +0300

    License notice cleanup

--- lib/Source.pm
+++ lib/Source.pm
@@ -1,12 +1,9 @@
-# -*- Mode: perl; indent-tabs-mode: nil -*-
-#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright (C) 2014 Jolla Ltd.
 # Contact: Pami Ketolainen <pami.ketolainen@jolla.com>
-
 
 =head1 NAME
 
