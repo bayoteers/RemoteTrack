c66baa0a02dea0dfca9c84d1b7be6c6d2684cffa
Pami Ketolainen <pami.ketolainen@jolla.com>
2017-04-23 20:12:31 +0300

    Update copyright

--- email_in_sync.pl
+++ email_in_sync.pl
@@ -3,10 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# This Source Code Form is "Incompatible With Secondary Licenses", as
-# defined by the Mozilla Public License, v. 2.0.
-#
-# Copyright (C) 2014 Jolla Ltd.
+# Copyright (C) 2014-2017 Jolla Ltd.
 # Contact: Pami Ketolainen <pami.ketolainen@jolla.com>
 
 use strict;
